c9f3128796cbdef19a6b2f482b3c2907f02b612b
AHMAD-FB <124088270+AHMAD-FB@users.noreply.github.com>
2025-12-17 14:56:16 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+OWNER_ID = 6231439063  # Replace with your Telegram user ID
+OFFICIAL_GROUP_LINK = "https://t.me/paivak"  # Replace with your group link
+AUTHORIZED_PRIVATE_USERS = set()  # Start empty, or preload user IDs
+AUTHORIZED_CHATS = set((-1003404040719,)) # Start empty, or preload chat IDs
+ADMIN_IDS = [6231439063]  # Replace with your real Telegram user ID
